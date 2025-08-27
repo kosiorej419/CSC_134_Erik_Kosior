ba9cc25d35fd000088476652bd5c29b207edf83c
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-08-27 11:58:23 -0400

    product_of_two_numbers finished

---
+++ product_of_two_numbers.cpp
@@ -0,0 +1,18 @@
+//@author: Erik Kosior
+//@date: 8/27/2025
+//@purpose: calculates the product of two numbers
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 5;
+    int num2 = 10;
+    int product = num1 * num2;
+
+    std::cout << product << std::endl;
+
+    return 0;
+}
