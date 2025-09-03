5544e75983e961e05c3336a288e77bda9b44849e
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 12:08:06 -0400

    distance_travelled complete

---
+++ week2Lab2/distance_travelled.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: calculate distance travelled
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int velocity = 80;
+    int time = 2;
+    int distance = velocity * time;
+
+    cout << "The distance travelled is " << distance << " miles.";
+}
