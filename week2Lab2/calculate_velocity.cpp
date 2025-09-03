649db2b0208a92a6e0fe945ed7191a3b81dd401d
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 12:12:34 -0400

    calculate_velocity complete

---
+++ week2Lab2/calculate_velocity.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: calculate velocity
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int time = 4;
+    int distance = 400;
+    int velocity = distance / time;
+
+    cout << "The velocity is " << velocity << " mph.";
+}
