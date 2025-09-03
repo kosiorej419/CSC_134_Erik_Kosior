81c266a6194e6da47848edf967e5c749a383c35f
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 16:49:34 -0400

    calculate_voltage complete

---
+++ week2Lab2/calculate_voltage.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: calculate voltage
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 10;
+    int r = 2;
+    int v = i * r;
+
+    cout << "The voltage is " << v << " volts.";
+}
