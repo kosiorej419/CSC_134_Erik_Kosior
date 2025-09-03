9addd89c048ddd32f811b4758e6ed8b6c10edd7e
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 16:55:11 -0400

    calculate_force complete

---
+++ week2Lab2/calculate_force.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: calculate force
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int m = 10;
+    double a = 9.81;
+    double f = m * a;
+
+    cout << "The force is " << f << " N.";
+}
