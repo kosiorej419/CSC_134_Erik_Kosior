9223e462ea05214c05de736f0d7f7b92024ac03c
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 12:02:04 -0400

    sqr_root complete

---
+++ week2Lab1/sqr_root.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: get the square root of a number
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int num = 4;
+    int sqr_root = sqrt(num);
+
+    cout << "The square root of 4 is " << sqr_root;
+}
