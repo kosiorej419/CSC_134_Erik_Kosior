cf9fd42c4ad65b21ad652ef7d78aa07f6a9501ec
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 11:43:50 -0400

    week3 lab complete

---
+++ average_july_high_temp.cpp
@@ -0,0 +1,23 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: find the new average july high temp
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //set original average temps
+    int new_york_temp = 85;
+    int denver_temp = 88;
+    int phoenix_temp = 105;
+
+    //get new average temps
+    double new_york_new_temp = new_york_temp * 1.02;
+    double denver_new_temp = denver_temp * 1.02;
+    double phoenix_new_temp = phoenix_temp * 1.02;
+
+    cout << "New average July high temperatures:\nNew York: " << new_york_new_temp << "\nDenver: " << denver_new_temp << 
+    "\nPhoenix: " << phoenix_new_temp;
+}
