b7f44ce0b19def0cdcbea0df3511661232678749
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-03 17:18:43 -0400

    favorite_movie complete

---
+++ week2Lab1/favorite_movie.cpp
@@ -0,0 +1,12 @@
+//@author: Erik Kosior
+//@date: 9/3/2025
+//@purpose: print my favorite movie and its main characters
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "My favorite movie is Jurassic Park\nMain characters:\nAlan Grant\nJohn Hammond\nIan Malcolm\nEllie Sattler";
+}
