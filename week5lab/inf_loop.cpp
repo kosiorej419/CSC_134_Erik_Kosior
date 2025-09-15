5f07344166ddb58e6de3c0bc0078021d2953d891
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-15 12:12:13 -0400

    inf_loop complete

---
+++ week5lab/inf_loop.cpp
@@ -0,0 +1,16 @@
+//@author: Erik Kosior
+//@date: 9/15/2025
+//@purpose: create an infinite loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    bool check = true;
+
+    while(check == true){
+        cout << "loop." << endl;
+    }
+}
