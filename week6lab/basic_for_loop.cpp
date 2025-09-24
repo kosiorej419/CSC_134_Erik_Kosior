53a3a2e3d45c79d1293bc4448c18be02b7fc61ce
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-24 11:09:26 -0400

    basic_for_loop complete

---
+++ week6lab/basic_for_loop.cpp
@@ -0,0 +1,15 @@
+//@author: Erik Kosior
+//@date: 9/24/2025
+//@purpose: simple for loop that goes through 10 iterations
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int count = 0; count < 10; count++)
+    {
+        cout << "This is iteration " << count + 1 << endl;
+    }
+}
