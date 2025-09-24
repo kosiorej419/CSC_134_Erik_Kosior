e77fcca878e6c30cb272b9f9eead4c778a92c10e
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-24 11:48:54 -0400

    reverse_loop complete

---
+++ week6lab/reverse_loop.cpp
@@ -0,0 +1,15 @@
+//@author: Erik Kosior
+//@date: 9/24/2025
+//@purpose: loop backward from 100 to 1 with for loop
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for(int i = 100; i > 0; i--)
+    {
+        cout << i << endl;
+    }
+}
