88a9f155900e4f29d567c1d066935f98729dbdaf
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-09-29 11:35:00 -0400

    multiplication_table v.1 complete

---
+++ week7lab1/multiplication_table.cpp
@@ -0,0 +1,85 @@
+//@author: Erik Kosior
+//@date: 9/29/2025
+//@purpose: use for loops to print a multiplication table of 1-10
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int multiple = 0;
+
+    for(int i = 1; i <= 1; i++)
+    {
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 1 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(int count = 1; count <= 10; count++)
+        {
+            multiple = 2 * count;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 3 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 4 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 5 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 6 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 7 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 8 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 9 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+
+        for(i = 1; i <= 10; i++)
+        {
+            multiple = 10 * i;
+            cout << multiple << "\t";
+        }
+        cout << endl;
+    }
+}
