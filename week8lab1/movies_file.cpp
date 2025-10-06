0c64f66f92febae446b4012c64e8248eda9015cf
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-10-06 11:40:19 -0400

    movies_file complete

---
+++ week8lab1/movies_file.cpp
@@ -0,0 +1,24 @@
+//@author: Erik Kosior
+//@date: 10/6/2025
+//@purpose: create a new file and write movie names to it
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream outputFile;
+    outputFile.open("lab1.txt");
+
+    outputFile << "Bourne Identity\n";
+    outputFile << "Star Wars Episode IV\n";
+    outputFile << "The Hunger Games\n";
+    outputFile << "Dead Pool\n";
+    outputFile << "X-Men First Class\n";
+    outputFile << "Star Wars Espisode V\n";
+    outputFile << "Get Out";
+    
+    outputFile.close();
+}
