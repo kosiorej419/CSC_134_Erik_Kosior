528c14feef586d88ee1e76b36561f01787495baa
Erik Kosior <kosiorej419@mail.sandhills.edu>
2025-11-17 11:56:53 -0500

    void_func complete

---
+++ week14lab/void_func.cpp
@@ -0,0 +1,20 @@
+//@author: Erik Kosior
+//@date: 11/17/2025
+//@purpose: use a void function to print a message
+
+#include <iostream>
+
+using namespace std;
+
+void displayWelcome(){
+    cout << "==============================\n";
+    cout << "\n";
+    cout << "  C++ Function Demonstration\n";
+    cout << "\n";
+    cout << "==============================" << endl;
+}
+
+int main()
+{
+    displayWelcome();
+}
